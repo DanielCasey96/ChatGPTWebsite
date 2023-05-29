6c86cbf51a13bded3cfef3273637c2724f02c2fe
spoona96 <dan_spooner15@hotmail.co.uk>
2023-05-29 01:08:53 +0100

    created a hidden button which displays the summary when clicked (was not fun)

--- scripts/chatgpt-website-blog.js
+++ scripts/chatgpt-website-blog.js
@@ -12,4 +12,28 @@ $(document).ready(function() {
       }
     });
   });
+
+  window.addEventListener('load', function() {
+    var expandBtn = document.getElementById('expandBtn');
+    var expandContent = document.getElementById('expandContent');
+  
+    expandBtn.addEventListener('click', function() {
+      if (expandContent.style.display === 'none') {
+        expandContent.style.display = 'block'; // Show the content
+        expandContent.style.maxHeight = expandContent.scrollHeight + 'px'; // Set the max height to the scroll height
+  
+        // Smooth scroll animation
+        expandContent.style.transition = 'max-height 0.3s ease-in-out';
+      } else {
+        expandContent.style.maxHeight = '0'; // Set the max height to 0
+  
+        // Delay hiding the content until the animation finishes
+        setTimeout(function() {
+          expandContent.style.display = 'none'; // Hide the content
+        }, 300); // Adjust the duration (in milliseconds) as needed
+      }
+    });
+  });
+  
+  
   
